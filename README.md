9df4b0a46f88b33fc8d304053994c9caa6c3dfbb
Hans Liss <Hans@Liss.pp.se>
2021-09-19 17:43:59 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # createetest
 Create exposure test file for the Sparkmaker FHD
+
+** How to use this
+Here's an example, assuming you want to test eight different exposure times from 8s to 15s:
+
+1. Load your test shape in Chitubox.
+2. Make a total of eight copies, and distribute them on the build plate (if you keep them tied together, it'll be easier to keep track of which one is which afterwards).
+3. Slice this for an exposure time of 8s, and save to a numbered file, as _etest_01.fhd_ (or _etest_08.fhd_, if you prefer that).
+4. Remove one copy - the one to receive the shortest exposure time.
+5. Slice again, and save as number _etest_02.fhd_ (or _etest_09.fhd_) - the exposure time is not relevant here.
+6. Redo 4-5 until only one copy of the shape is left.
+7. Now you should have eight files, which when listed ends up in numerical order
+8. Now run the program, and generate the test file!
+
+```
+./createetest -b 8 -d 1 -o etest.fhd etest_??.fhd
+```
+
+Run the program without parameters to see usage info.
+
+** TODO
+Currently, the program assumes a lot. It assumes that the structure of a single layer, apart from Z axis movement, is fixed:
+
+```
+;dataSize:19354
+{{
+<PNG data>
+}}
+M106 S255;
+G4 S100;
+;L:2;
+M106 S0;
+```
+
+If Chitubox starts producing different G-code, this program will have to be modified to accommodate that.
+
+Additionally, it blindly assumes that all the files have exactly the same structure.
+If you follow the instructions, that's not going to be a problem.
+However, we really only need the image blobs from all the input files apart from the first one, so we could
+just as easily scan those files until the ";dataSize" header appears and use that to extract the PNG, and
+ignore all other rows, copying the necessary commands from the first file instead of reading them from each file.
---
+++ createetest.c
@@ -0,0 +1,182 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<ctype.h>
+
+#define BUFSIZE 262144
+
+#define SDATASIZE ";dataSize:"
+#define SZ_SDATASIZE (sizeof(SDATASIZE) - 1)
+
+void usage(char *p) {
+  fprintf(stderr, "Usage: %s -b <base time> -d <delta time> -o <output file> [-c <current time, default 8>] <file> ...\n", p);
+}
+
+int main(int argc, char *argv[]) {
+  FILE **infiles=NULL;
+  FILE *outfile=NULL;
+  int nfiles=0;
+  double baseTime=-1;
+  double deltaTime=-1;
+  int currentTime=8;
+  double waitTime;
+  int o, i, pngSize, ready=0, isBaselayer;
+  static char inbuf[BUFSIZE];
+  while ((o=getopt(argc, argv, "b:d:c:o:")) != -1) {
+    switch(o) {
+    case 'b':
+      baseTime=atof(optarg);
+      break;
+    case 'd':
+      deltaTime=atof(optarg);
+      break;
+    case 'c':
+      currentTime=atoi(optarg);
+      break;
+    case 'o':
+      if (!(outfile = fopen(optarg, "wb"))) {
+	perror(optarg);
+	return -2;
+      }
+      break;
+    default:
+      usage(argv[0]);
+      return -1;
+      break;
+    }
+  }
+  if (baseTime == -1 || deltaTime == -1) {
+    usage(argv[0]);
+    return -1;
+  }
+
+  nfiles = argc - optind;
+  infiles = (FILE **)malloc(nfiles * sizeof(FILE *));
+  memset(infiles, 0, nfiles * sizeof(FILE *));
+  for (i = 0; i < nfiles; i++) {
+    if (!(infiles[i] = fopen(argv[i + optind], "rb"))) {
+      perror(argv[i + optind]);
+      return -2;
+    }
+  }
+
+  if (nfiles < 2) {
+    usage(argv[0]);
+    return -3;
+  }
+
+  while (!ready) {
+    if (!fgets(inbuf, BUFSIZE, infiles[0])) {
+      ready = 1;
+    } else {
+      if (strncmp(inbuf, SDATASIZE, SZ_SDATASIZE) != 0) {
+	// For normal lines, just copy to outfile and consume one line from each one
+	// of the other files.
+	// Note: This is going to be massively messed up if the files don't have the
+	// same structure. It's way too much work to verify each line.
+	fputs(inbuf, outfile);
+	for (i = 1; i<nfiles; i++) {
+	  if (!fgets(inbuf, BUFSIZE, infiles[i])) {
+	    fprintf(stderr, "Short file #%d.\n", i);
+	    return -4;
+	  }
+	}
+      } else {
+	// ;dataSize:15864
+	for (i = 0; i<nfiles; i++) {
+	  if (i > 0) {
+	    fgets(inbuf, BUFSIZE, infiles[i]);
+	  }
+	  // Handle the dataSize line.
+	  // First copy it to the output file
+	  if (!(isBaselayer && i > 0)) {
+	    fputs(inbuf, outfile);
+	  }
+	  
+	  // Insert a NUL after the last digit, by searching backwards.
+	  while (strlen(inbuf) && !isdigit(inbuf[strlen(inbuf) - 1])) {
+	    inbuf[strlen(inbuf) - 1] = '\0';
+	  }
+	  
+	  // Unnecessary sanity check that will be incredibly important if left out
+	  if (strlen(inbuf) <= SZ_SDATASIZE) {
+	    fprintf(stderr, "This cannot happen #1\n");
+	    return -999;
+	  }
+	  
+	  // Read the size of the PNG data
+	  pngSize = atoi(&inbuf[SZ_SDATASIZE]);
+	  
+	  // {{
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+	  if (!(isBaselayer && i > 0)) {
+	    fputs(inbuf, outfile);
+	  }
+	  
+	  // PNG data
+	  fread(inbuf, 1, pngSize, infiles[i]);
+	  if (!(isBaselayer && i > 0)) {
+	    fflush(outfile);
+	    fwrite(inbuf, 1, pngSize, outfile);
+	    fputs("\n", outfile);
+	  }
+	  
+	  // There's a newline after the PNG file.
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+	  
+	  // }}
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+	  if (!(isBaselayer && i > 0)) {
+	    fputs(inbuf, outfile);
+	  }
+	  
+	  // M106 S255;
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+	  if (!(isBaselayer && i > 0)) {
+	    fputs(inbuf, outfile);
+	  }
+	  
+	  // G4 S100;
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+	  // Insert a NUL after the last digit, by searching backwards.
+	  while (strlen(inbuf) && !isdigit(inbuf[strlen(inbuf) - 1])) {
+	    inbuf[strlen(inbuf) - 1] = '\0';
+	  }
+	  waitTime = atof(inbuf + 4);
+	  if ((int)waitTime == currentTime) {
+	    waitTime = (i == 0)?baseTime:deltaTime;
+	    isBaselayer = 0;
+	  } else {
+	    isBaselayer = 1;
+	  }
+	  if (!(isBaselayer && i > 0)) {
+	    fprintf(outfile, "G4 S%.3lf;\n", waitTime);
+	  }
+
+	  // ;L:2;
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+
+	  // ;M106 S0;
+	  fgets(inbuf, BUFSIZE, infiles[i]);
+	  if (!(isBaselayer && i > 0)) {
+	    fputs(inbuf, outfile);
+	  }
+	}
+      }
+    }
+  }
+  if (outfile) {
+    fclose(outfile);
+  }
+  if (infiles) {
+    for (i = 0; i < nfiles; i++) {
+      if (infiles[i]) {
+	fclose(infiles[i]);
+	infiles[i] = NULL;
+      }
+    }
+    free(infiles);
+    infiles = NULL;
+  }
+}
